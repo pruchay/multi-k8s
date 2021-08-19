07773bf3d2f239beef746d335668498b0be26338
ipruchai <i.pruchai@sportradar.com>
2021-08-19 14:19:26 +0200

    Update version

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1>Fib calculator v2</h1>
+            <h1>Fib calculator version KUBERNETES!</h1>
             <p>
               Scroll down ----->
             </p>
